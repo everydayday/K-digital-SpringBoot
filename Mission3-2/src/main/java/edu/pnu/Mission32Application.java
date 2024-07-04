533b0607265033e07cc5cef20d2f2153eff6be87
everydayday <96685431+everydayday@users.noreply.github.com>
2024-07-04 18:45:28 +0900

    mission3-3

---
+++ Mission3-2/src/main/java/edu/pnu/Mission32Application.java
@@ -0,0 +1,13 @@
+package edu.pnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mission32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mission32Application.class, args);
+	}
+
+}
